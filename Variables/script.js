4e06b77beb53020ee82fb0e47daa40fb4bb19e45
Sridhar Hisaria <83956232+mystic18pro@users.noreply.github.com>
2024-07-15 09:41:34 +0000

    video 2 (variables) + some QoL changes

---
+++ Variables/script.js
@@ -0,0 +1,12 @@
+let fullName = "Sridhar";
+let age = 13;
+let inSchool = true;
+
+p1 = document.getElementById("p1")
+p2 = document.getElementById("p2")
+p3 = document.getElementById("p3")
+
+
+p1.textContent = `Your name is ${fullName}`
+p2.textContent = `You are ${age} years old`
+p3.textContent = inSchool
--- script.js
+++ script.js
@@ -3,3 +3,20 @@ var a = 'Hello';
 console.log(`Variable a is ${a}`);
 
 document.getElementById(`myH1`).textContent = "Hello";
+
+let videos = ["Introduction", "Variables", "Arithmetic Operators"];
+let list = document.getElementById("videoList");
+for (let i = 0; i < videos.length; ++i) {
+    let li = document.createElement('li');
+    if (i === 0) {
+        // For the first item, just set the text
+        li.innerText = videos[i];
+    } else {
+        // For the other items, create an anchor tag
+        let a = document.createElement('a');
+        a.href = videos[i] + '/index.html';
+        a.innerText = videos[i];
+        li.appendChild(a);
+    }
+    list.appendChild(li);
+}
