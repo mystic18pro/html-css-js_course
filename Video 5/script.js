16ee0184014ae92ea8a28f9325d4029245daa20c
Sridhar Hisaria <83956232+mystic18pro@users.noreply.github.com>
2024-07-21 08:18:53 +0000

    video 5

---
+++ Video 5/script.js
@@ -0,0 +1,25 @@
+// let age = window.prompt("Enter your age: ");
+// document.getElementById("original").textContent=`Original: ${age}`;
+
+
+// console.log(typeof age);
+// console.log(age+1);
+// document.getElementById("myh1").textContent=`${typeof age}: ${age+1}`;
+
+// age = Number(age);
+// console.log(typeof age);
+// document.getElementById("myh1two").textContent=`${typeof age}: ${age+1}`;
+// console.log(age+1);
+
+
+let x = "";
+let y = "";
+let z = "";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
